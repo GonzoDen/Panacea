e027b34ed9c14462fc7b93e9d452b32a1480305a
GonzoDen <deniz.nazarova.99@gmail.com>
2019-06-24 16:05:42 +0600

    Rearranging into "UnityProject"

---
+++ UnityProject/Panacea/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+    public float speed = 3;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+        Debug.Log(direction);
+        rigidbody2D.velocity = new Vector2(direction * speed, rigidbody2D.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Debug.Log("POW!");
+    }
+
+
+}
