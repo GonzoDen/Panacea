69db4483a139b46367d0a4e8ed323066cb449bb2
GonzoDen <deniz.nazarova.99@gmail.com>
2019-06-25 15:29:51 +0600

    Added EnemySpawner: - Created the object that spawns enemies - Added special features (SpawnTime, EnemyVelocity)

---
+++ UnityProject/Panacea/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyObject;
+    public float spawnTime=3f;
+    private float timeBeforeSpawn;
+    public float enemyVelocity = -5f;
+    void Start()
+    {
+        timeBeforeSpawn = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBeforeSpawn > 0) timeBeforeSpawn -= Time.deltaTime;
+        SpawnEnemy();
+    }
+
+    void SpawnEnemy()
+    {
+        if (timeBeforeSpawn <= 0)
+        {
+            GameObject newEnemy = Instantiate(enemyObject,this.transform.position, Quaternion.identity);
+            Rigidbody2D enemyRb2d = newEnemy.GetComponent<Rigidbody2D>();
+            enemyRb2d.gravityScale = 0;
+            enemyRb2d.velocity = new Vector2(0, enemyVelocity);
+            timeBeforeSpawn = spawnTime;
+        }
+    }
+}
--- UnityProject/Panacea/Assets/Scripts/Gun.cs
+++ UnityProject/Panacea/Assets/Scripts/Gun.cs
@@ -8,18 +8,24 @@ public class Gun : MonoBehaviour
     public GameObject prefabBlue;
     public KeyCode bulletRedButton;
     // public KeyCode bulletBlueButton;
+    ParticleSystem ps;
+    ParticleSystem.EmitParams emitParams;
 
     void Start()
     {
-
+        ps = GetComponent<ParticleSystem>();
+        // emitParams = new ParticleSystem.EmitParams();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // ps.Emit(emitParams, 50);
+        
         if (Input.GetKeyDown(bulletRedButton))
         {
             GameObject projectile = Instantiate(prefabRed, transform.position, Quaternion.identity);
+            ps.Play();
             Destroy(projectile, 3);
 
         }
@@ -29,10 +35,13 @@ public class Gun : MonoBehaviour
             GameObject projectileBullet1 = Instantiate(prefabBlue, transform.position, Quaternion.identity);
             GameObject projectileBullet2 = Instantiate(prefabBlue, transform.position, Quaternion.identity);
             GameObject projectileBullet3 = Instantiate(prefabBlue, transform.position, Quaternion.identity);
+            ps.Play();
             Destroy(projectileBullet1, 3);
             Destroy(projectileBullet2, 3);
             Destroy(projectileBullet3, 3);
 
         }
+
+        
     }
 }
